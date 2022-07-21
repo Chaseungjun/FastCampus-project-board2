c409788ab38a18c8e10dc2b6850a156edd6bf9d6
Chaseoungjun <chas369@naver.com>
2022-07-21 14:39:14 +0900

    #1 - QueryDsl 지원을 이용하여 API 검색옵션 추가 'QuerydslPredicateExecutor' , 'QuerydslBinderCustomizer' 를 추가

--- src/main/java/com/fastcampus/repository/ArticleCommentRepository.java
+++ src/main/java/com/fastcampus/repository/ArticleCommentRepository.java
@@ -1,9 +1,29 @@
 package com.fastcampus.repository;
 
 import com.fastcampus.projectboard2.domain.ArticleComment;
+import com.fastcampus.projectboard2.domain.QArticle;
+import com.fastcampus.projectboard2.domain.QArticleComment;
+import com.querydsl.core.types.dsl.StringExpression;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.querydsl.binding.QuerydslBinderCustomizer;
+import org.springframework.data.querydsl.binding.QuerydslBindings;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
-public interface ArticleCommentRepository extends JpaRepository<ArticleComment, Long> {
+public interface ArticleCommentRepository extends
+        JpaRepository<ArticleComment, Long>,
+        QuerydslPredicateExecutor<ArticleComment>,
+        QuerydslBinderCustomizer<QArticleComment> {
+
+
+    @Override
+    default void customize(QuerydslBindings bindings, QArticleComment root){ // 인터페이스 안에서는 메서드를 구현할 수 없지만 자바 8부터 가능해짐 (default 메서드)
+        bindings.excludeUnlistedProperties(true); // 리스틩을 하지 않은 프로퍼티는 검색 대상에서 제외
+        bindings.including(root.content, root.createdAt, root.createdBy); // 검색 조건
+
+        bindings.bind(root.content).first(StringExpression::containsIgnoreCase);
+        bindings.bind(root.createdBy).first(StringExpression::containsIgnoreCase);
+//        bindings.bind(root.createdAt).first(StringExpression::eq);
+    }
 }
