24513c8e31f7a29cc2fbf105ab790edfb6c534f0
Chaseoungjun <chas369@naver.com>
2022-07-23 19:41:25 +0900

    회원 계정 도메인 구현 및 erd 반영 `UserAccount` 이름으로 회원 계정 도메인 생성 `user`, `account` 등은 mysql 예약어이므로 피함 필드명도 mysql 예약어를 의식하여 정함
    테스트 데이터에 임의 계정 1개 등록
    테스트용이지만 패스워드가 노출되는 방식이므로 고민 필요
    
    회원 정보는 API 로 노출되지 않았으면 한다.
    이에 테스트에 404 not found 를 확인하는 테스트 추가
    간단하게 모든 http 메소드에 대응

--- src/test/java/com/fastcampus/projectboard2/controller/DataRestTest.java
+++ src/test/java/com/fastcampus/projectboard2/controller/DataRestTest.java
@@ -75,4 +75,18 @@ public class DataRestTest {
                 .andExpect(status().isOk())
                 .andExpect(content().contentType(MediaType.valueOf("application/hal+json"))); // MediaType 검사에는 HaL json 타입이 없으므로 valueOf로 검사
     }
+
+    @DisplayName("[api] 회원 관련 API 는 일체 제공하지 않는다.")
+    @Test
+    void givenNothing_whenRequestingUserAccounts_thenThrowsException() throws Exception {
+        // Given
+
+        // When & Then
+        mockMvc.perform(get("/api/userAccounts")).andExpect(status().isNotFound());
+        mockMvc.perform(post("/api/userAccounts")).andExpect(status().isNotFound());
+        mockMvc.perform(put("/api/userAccounts")).andExpect(status().isNotFound());
+        mockMvc.perform(patch("/api/userAccounts")).andExpect(status().isNotFound());
+        mockMvc.perform(delete("/api/userAccounts")).andExpect(status().isNotFound());
+        mockMvc.perform(head("/api/userAccounts")).andExpect(status().isNotFound());
+    }
 }
